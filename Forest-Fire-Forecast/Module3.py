5c15432e4e53100cebc521c3643c2e9e0c92c79c
aisaav <aisaav@umich.edu>
2020-07-14 13:04:10 -0400

    Style(Forest-Fire-Forecast): Made changes to follow PEP8 guidelines

--- Forest-Fire-Forecast/Module3.py
+++ Forest-Fire-Forecast/Module3.py
@@ -2,5 +2,5 @@ import pandas
 filename='dataset/forestfires.csv'
 names = ['X', 'Y', 'month', 'day', 'FFMC', 'DMC',
          'DC', 'ISI', 'temp', 'RH', 'wind','rain', 'area']
-df= pandas.read_csv(filename,names=names)
+df = pandas.read_csv(filename,names=names)
 print(pandas.isnull(df))
